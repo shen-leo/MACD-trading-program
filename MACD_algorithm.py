218d94a60d6d656c97d3a8316363f627bf0acd26
MangoMelona <shen.leo@outlook.com>
2022-04-07 01:22:44 -0700

    add plot function

--- MACD_algorithm.py
+++ MACD_algorithm.py
@@ -115,16 +115,80 @@ def plot_trade_list(ticker, historical_data, ticker_macd, buy_price, sell_price)
     plt.show()
 
 
+# function to create a position with MACD
+def create_position(macd_signal, historical_data, ticker_macd):
+    position = []
+    for i in range(len(macd_signal)):
+        if macd_signal[i] > 1:
+            position.append(0)
+        else:
+            position.append(1)
+
+    for i in range(len(historical_data['close'])):
+        if macd_signal[i] == 1:
+            position[i] = 1
+        elif macd_signal[i] == -1:
+            position[i] = 0
+        else:
+            position[i] = position[i - 1]
+
+    macd = ticker_macd['macd']
+    signal = ticker_macd['signal']
+    close_price = historical_data['close']
+    macd_signal = pd.DataFrame(macd_signal).rename(columns={0: 'macd_signal'}).set_index(historical_data.index)
+    position = pd.DataFrame(position).rename(columns={0: 'macd_position'}).set_index(historical_data.index)
+
+    frames = [close_price, macd, signal, macd_signal, position]
+    strategy = pd.concat(frames, join='inner', axis=1)
+
+    return strategy
+
+
+# function for back testing the model
+def back_testing(ticker, strategy, historical_data, investment_value):
+    ticker_return = pd.DataFrame(np.diff(historical_data['close'])).rename(columns={0: 'returns'})
+    macd_strategy_ret = []
+
+    for i in range(len(ticker_return)):
+        try:
+            returns = ticker_return['returns'][i] * strategy['macd_position'][i]
+            macd_strategy_ret.append(returns)
+        except ValueError:
+            pass
+
+    macd_strategy_ret_df = pd.DataFrame(macd_strategy_ret).rename(columns={0: 'macd_returns'})
+
+    number_of_stocks = floor(investment_value / historical_data['close'][0])
+    macd_investment_ret = []
+
+    for i in range(len(macd_strategy_ret_df['macd_returns'])):
+        returns = number_of_stocks * macd_strategy_ret_df['macd_returns'][i]
+        macd_investment_ret.append(returns)
+
+    macd_investment_ret_df = pd.DataFrame(macd_investment_ret).rename(columns={0: 'investment_returns'})
+    total_investment_ret = round(sum(macd_investment_ret_df['investment_returns']), 2)
+    profit_percentage = floor((total_investment_ret / investment_value) * 100)
+    print(cl('Profit gained from the MACD strategy by investing $100k in {} : {}'.format(ticker, total_investment_ret),
+             attrs=['bold']))
+    print(cl('Profit percentage of the MACD strategy : {}%'.format(profit_percentage), attrs=['bold']))
+
+
 # main function to drive the program
 def main():
     ticker = input("Stock Ticker: ").upper()
     date = input("Date: ")
+    investment_value = int(input("Investment Value: "))
+
     historical_data = get_historical_data(ticker, date)
     ticker_macd = calculate_macd(historical_data['close'], 26, 12, 9)
     ticker_macd.tail()
+
     plot_macd(historical_data['close'], ticker_macd['macd'], ticker_macd['signal'], ticker_macd['hist'])
     buy_price, sell_price, macd_signal = implement_macd_strategy(historical_data['close'], ticker_macd)
     plot_trade_list(ticker, historical_data, ticker_macd, buy_price, sell_price)
+
+    strategy = create_position(macd_signal, historical_data, ticker_macd)
+    back_testing(ticker, strategy, historical_data, investment_value)
 
 
 if __name__ == "__main__":
